17a577bc2af6e9560de5d9f60533bcf0344ae6f3
Upendra Kumar <toxic74412@gmail.com>
2024-09-29 17:04:01 +0530

    creating api to get_all and get_product

---
+++ store/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from .models import Product 
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField()
+    title = serializers.CharField(max_length=255)
+    price = serializers.DecimalField(max_digits=6,decimal_places=2)
+
+    class Meta:
+        model = Product    
+        fields = ["id", "title", "price"]    
+    
+
+        
